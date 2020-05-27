6772e2f780c09f2a6c58a5e049d3b43dbaf68651
tomatochocolate <921797770@qq.com>
2020-05-27 18:02:46 +0800

    5.27

--- src/router/modules/statistics.js
+++ src/router/modules/statistics.js
@@ -8,7 +8,7 @@ export default [
         path: '/statistics',
         meta: {
             icon: 'ios-analytics',
-            title: '未开通',
+            title: '运营统计',
             permissions: [ 'guest' ]
         },
         component: Main,
@@ -19,7 +19,7 @@ export default [
                 meta: {
                     menu: false
                 },
-                component: () => import(/* webpackChunkName: "statistics" */'@/views/other/develop')
+                component: () => import(/* webpackChunkName: "statistics" */'@/views/statistics')
             }
         ]
     }
